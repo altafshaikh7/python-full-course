3e5ab2c7313ec1f19dea52850dc46b4c4a92d1ef
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-16 20:17:30 +0530

    chapter 5 and chapter 6

--- chapter-6/chapter-6-ps.py
+++ chapter-6/chapter-6-ps.py
@@ -1,18 +1,18 @@
 
 #write a program to find the greatest using if else ladder from user input
 
-# a1=int(input("Enter first number: " ))
-# a2=int(input("Enter second number: " ))
-# a3=int(input ("enter third number: " ))
+a1=int(input("Enter first number: " ))
+a2=int(input("Enter second number: " ))
+a3=int(input ("enter third number: " ))
 
-# if (a1>a2 and a1>a3):
-#     print("greatest number of a1" , a1)
+if (a1>a2 and a1>a3):
+    print("greatest number of a1" , a1)
 
-# elif(a2>a1 and a2>a3):
-#     print("greatest number of a2", a2)
+elif(a2>a1 and a2>a3):
+    print("greatest number of a2", a2)
 
-# elif(a3>a1 and a3>a2):
-#     print("greatest number of a3", a3 )
+elif(a3>a1 and a3>a2):
+    print("greatest number of a3", a3 )
 
 
 
