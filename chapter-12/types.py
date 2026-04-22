af5157b9018c1472be62b15eda35d0d47eba6ead
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-22 13:04:49 +0530

    chapter 12

---
+++ chapter-12/types.py
@@ -0,0 +1,13 @@
+from typing import list ,Union,Tuple
+
+
+# for list of integar
+number: list [int]=[1,2,23,50]
+
+# tuple of string and integar 
+person: Tuple[str,int]=("John",30)
+
+# Union of string and integar
+data: Union[str,int]= "Hello"
+
+
