01bcea54069663f86ed0b4bd4fbff31b132f84c7
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-23 00:15:46 +0530

    chapter 12

--- chapter-12/list-comprehension.py
+++ chapter-12/list-comprehension.py
@@ -1,1 +1,10 @@
 
+mylist=[1,5,6,7,8,9]
+squaredlist=[]
+
+# for item in mylist:
+    # squaredlist.append(item*item)
+
+#this can be simplified using list comprehension 
+squaredlist=[i*i for i in mylist]
+print(squaredlist)
