5adf143b08b4ffb28fd4bc782cc6973e3e7f7b03
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-23 10:03:04 +0530

    chapter 13

---
+++ chapter-13/join.py
@@ -0,0 +1,14 @@
+ 
+a=["hello", "world", "python"]
+
+final = " - ".join(a)
+print (final)
+
+b=["1", "2", "3", "4", "5"]
+final = " + ".join(b)
+print (final)
+
+
+c=["apple", "banana", "cherry"]
+final = " and ".join(c)
+print (final)
